3f4013ba114abae746322c8f293023ff5e3520bf
Jadon Orr <jadonorr01@gmail.com>
2018-06-08 13:41:02 -0400

    Refactored return largest numbers

--- basic-algorithms/returnLargestNumbers.js
+++ basic-algorithms/returnLargestNumbers.js
@@ -1,7 +1,5 @@
 function largestOfFour(arr) {
-  const largest = arr.map(nums => Math.max(...nums));
-
-  return largest;
+  return arr.map(nums => Math.max(...nums));
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
