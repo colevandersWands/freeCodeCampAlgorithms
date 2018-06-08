7a40c7570c42884b0aa8269ec47c6aa3fc8eb8b3
Jadon Orr <jadonorr01@gmail.com>
2018-06-08 11:04:54 -0400

    Refactored title case a sentence

--- basic-algorithms/titleCaseASentence.js
+++ basic-algorithms/titleCaseASentence.js
@@ -1,20 +1,5 @@
 function titleCase(str) {
-    str = str.toLowerCase();
-    var words = str.split(/ /g);
-    var capLetters = [];
-    var wordsNoFirstLetter = [];
-    var titleCaseWords = [];
-    
-    for (var i = 0; i < words.length; i++) {
-      capLetters.push(words[i][0].toUpperCase());
-      wordsNoFirstLetter.push(words[i].slice(1));
-      titleCaseWords.push(capLetters[i] + wordsNoFirstLetter[i]);
-    }
-    
-    var titleCaseSentence = titleCaseWords.join(" ");
-    
-    return titleCaseSentence;
-  }
-  
-  titleCase("I'm a little tea pot");
-  
+  return str.toLowerCase().split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+
+titleCase("I'm a little tea pot");
